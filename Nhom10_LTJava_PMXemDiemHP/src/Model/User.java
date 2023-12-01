23c318397e73c16d9f6fb610aa56306327ff4f06
manhdung02 <vmd111202@gmail.com>
2023-12-01 11:18:49 +0700

    new

---
+++ Nhom10_LTJava_PMXemDiemHP/src/Model/User.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author 84362
+ */
+public class User {
+    private String ID;
+    private String Ten;
+    private String MatKhau;
+    private String Vaitro;
+    private String Email;
+    private String DiaChi;
+    
+}
